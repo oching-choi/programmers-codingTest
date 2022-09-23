3ef245bffdf1e9175aa30127952ee684cdb3081e
oching-choi <oching.choi@gmail.com>
2022-09-23 12:33:31 +0900

    lv1. 서울에서 김서방 찾기

---
+++ lv1-13.js
@@ -0,0 +1,9 @@
+//programmers coding test 
+//lv1. 서울에서 김서방 찾기
+//https://school.programmers.co.kr/learn/courses/30/lessons/12919
+
+function solution(seoul) {
+    var answer = '';
+    answer = `김서방은 ${seoul.indexOf("Kim")}에 있다`
+    return answer;
+}
